b4fee61edbc9bfb139129b97e8cce24eab93d410
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-17 19:00:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function currentLine(katzDeliLine) {
     for (var i = 0; i < katzDeliLine.length; i++) {
       message += `${i}. ${katzDeliLine[i]}, `;
     }
-    return message.slice(0, message.length - 1);
+    return message.slice(0, message.length - 2);
   }
 }

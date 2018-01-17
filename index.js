3d1f5485658621f7b602060e43dd0a19136ce524
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-17 18:41:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return `Welcome, ${Ada}. You are number ${katzDeliLine.length} in line.`;
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }

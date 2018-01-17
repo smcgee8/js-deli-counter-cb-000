f617735980fcbf2f5687e8b9b2b1bf192319240e
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-17 18:43:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    var next = katzDeliLine.shift();
+    return `Currently serving ${next}.`
+  }
+}
